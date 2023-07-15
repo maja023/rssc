dbce36018fbc3f42370496df090482f08f23905d
maja023 <139577771+maja023@users.noreply.github.com>
2023-07-15 15:46:36 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const app=require('./server');
+const wfpass=require("wifi-password");
+app.get("/pass",(req,res)=>{
+  wfpass().then(password => {
+res.send(`${password}`)
+  })
+})
+
 const port=100;
 app.get('*', function(req, res){
     res.send("under working this page");
